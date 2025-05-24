b03b9edc16e619554de7f32c6411fb9bffc3ef15
emmy <52672675+emmaccode@users.noreply.github.com>
2025-05-24 14:19:47 -0600

    working, near finalizing, version
    The last big thing this project needs is multi-threading support, which should actually be as easy as adding arguments to `start!` i would also like to add support for configuration files using `save_config` and `load_config`

--- src/ParametricProcessScheduler.jl
+++ src/ParametricProcessScheduler.jl
@@ -4,15 +4,17 @@ using ParametricProcesses
 using Dates
 
 mutable struct RecurringTime
-    start::DateTime
+    start_time::DateTime
     interval::Dates.Period
 end
 
+next_time(now::DateTime, date::DateTime) = date
+
 function next_time(now::DateTime, rt::RecurringTime)
-	if ref_time < rt.start_time
+	if now < rt.start_time
 		return rt.start_time
 	end
-	delta = ref_time - rt.start_time
+	delta = now - rt.start_time
 	n_intervals = floor(Int, Millisecond(delta) / Millisecond(rt.interval))
 	return rt.start_time + (n_intervals + 1) * rt.interval
 end
@@ -22,17 +24,62 @@ mutable struct Task{T <: Any} <: ParametricProcesses.AbstractJob
     job::ParametricProcesses.ProcessJob
 end
 
+function new_task(f::Function, t::Any, args ...; keyargs ...)
+    Task{typeof(t)}(t, new_job(f, args ...; keyargs ...))
+end
+
+function clean!(workers::Workers{<:Any}, task::Task{<:Any}, taske::Int64)
+end
+
+function clean!(workers::Workers{<:Any}, task::Task{DateTime}, taske::Int64)
+    @info "deleted task"
+    deleteat!(workers, taske)
+end
+
 mutable struct Scheduler <: ParametricProcesses.AbstractProcessManager
     jobs::Vector{Task}
-    workers::ParametricProcesses.Workers{<:Any}
+    workers::Vector{Worker}
     active::Bool
+    Scheduler(tasks::Task ...) = new([tasks ...], Vector{Worker}(), false)
+end
+
+function next_time(now::DateTime, sched::Scheduler)
+    times = [next_time(now, task.time) for task in sched.jobs]
+    findmin(times)
 end
 
 function start(scheduler::Scheduler)
-    next_task = 1
+    current_dtime = now()
+    next_task, taske = next_time(current_dtime, scheduler)
+    scheduler.active = true
     t = @async while scheduler.active
-
+        current_dtime = Dates.now()
+        if current_dtime >= next_task
+            @info "performing task"
+            assign_open!(scheduler, scheduler.jobs[taske].job)
+            clean!(scheduler.jobs, scheduler.jobs[taske], taske)
+            sleep(1)
+            next_task, taske = next_time(current_dtime, scheduler)
+        end
+        yield()
     end
+    w = Worker{Async}("scheduler", rand(1000:3000))
+    w.active = scheduler.active
+    push!(scheduler.workers, w)
+    @info "started scheduler"
+    scheduler::Scheduler
+end
+
+function assign_open!(pm::Scheduler, job::ParametricProcesses.AbstractJob; not = Async)
+    ws = pm.workers
+    ws = filter(w -> typeof(w) != Worker{not}, ws)
+    open = findfirst(w -> ~(w.active), ws)
+    if ~(isnothing(open))
+        w = ws[open]
+        assign!(w, job)
+        return([w.pid])
+    end
+    job.f(job.args ...; job.kwargs ...)
 end
 
 function start(path::String = pwd() * "config")
@@ -43,5 +90,5 @@ function save_config(sch::Scheduler)
 
 end
 
-
+export new_task, scheduler
 end # module ParametricProcessScheduler
